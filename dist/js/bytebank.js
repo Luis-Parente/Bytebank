3649559d35702c6c785f68a82c664d3f53fb0719
Luis Parente <lgsp25@hotmail.com>
2025-11-02 10:37:40 -0300

    projeto reestruturado e adicionado tsconfig

--- dist/js/bytebank.js
+++ dist/js/bytebank.js
@@ -1,21 +1,21 @@
-var saldo = 3000;
-var elementoSaldo = document.querySelector('.saldo-valor .valor');
+let saldo = 3000;
+const elementoSaldo = document.querySelector('.saldo-valor .valor');
 if (elementoSaldo) {
     elementoSaldo.textContent = saldo.toString();
 }
-var elementoFormulario = document.querySelector(".block-nova-transacao form");
+const elementoFormulario = document.querySelector(".block-nova-transacao form");
 elementoFormulario.addEventListener("submit", function (event) {
     event.preventDefault();
     if (!elementoFormulario.checkValidity()) {
         alert("Por favor preencha todos os campos!");
         return;
     }
-    var inputTipoTransacao = document.querySelector('#tipoTransacao');
-    var inputValor = document.querySelector('#valor');
-    var inputData = document.querySelector('#data');
-    var tipoTransacao = inputTipoTransacao.value;
-    var valor = inputValor.valueAsNumber;
-    var data = new Date(inputData.value);
+    const inputTipoTransacao = document.querySelector('#tipoTransacao');
+    const inputValor = document.querySelector('#valor');
+    const inputData = document.querySelector('#data');
+    let tipoTransacao = inputTipoTransacao.value;
+    let valor = inputValor.valueAsNumber;
+    let data = new Date(inputData.value);
     if (tipoTransacao == "Depósito") {
         saldo += valor;
     }
@@ -27,7 +27,7 @@ elementoFormulario.addEventListener("submit", function (event) {
         return;
     }
     elementoSaldo.textContent = saldo.toString();
-    var novaTransacao = {
+    const novaTransacao = {
         tipoTransacao: tipoTransacao,
         valor: valor,
         data: data,
